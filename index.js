77a4931102756fa95ef4de7be84fcb2d4dd540e6
Aayush Bhattarai <bhattaraiaayush.78@gmail.com>
2023-10-16 10:54:28 +0300

    roll a dice exercise

--- index.js
+++ index.js
@@ -171,6 +171,35 @@ document.getElementById("increaseBtn").onclick = function(){
 
 //Exercise: Random Number generator
 
-let x = Math.floor(Math.random()*6) +1 ;  // 6 sided dice
+// let x = Math.floor(Math.random()*6) +1 ;  // 6 sided dice
 
-console.log(x);
+/* let x;
+document.getElementById("rollBtn").onclick = function(){
+    x = Math.floor(Math.random() * 6) + 1;  // 6 sided dice
+    document.getElementById("xLabel").innerHTML = x;
+    console.log(x);
+}; */
+
+/* let a;
+document.getElementById("rollButton").onclick = function(){
+
+    a = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("xlabel").innerHTML = a;
+    
+} */
+
+let x;
+let y;
+let z;
+
+document.getElementById("rollButton").onclick = function(){
+
+    x = Math.floor(Math.random() * 6) + 1;
+    y = Math.floor(Math.random() * 6) + 1;
+    z = Math.floor(Math.random() * 6) + 1;
+
+    document.getElementById("xlabel").innerHTML = x;
+    document.getElementById("ylabel").innerHTML = y;
+    document.getElementById("zlabel").innerHTML = z;
+}
+
