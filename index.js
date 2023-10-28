5eddc456a9c020fe2c7abe726848eecd14198eaa
Aayush Bhattarai <bhattaraiaayush.78@gmail.com>
2023-10-28 23:46:27 +0300

    string slicing

--- index.js
+++ index.js
@@ -225,3 +225,26 @@ userName = userName.toLowerCase();  */
 /* phoneNumber = phoneNumber.replaceAll("-", "*")   //--> replacing given string into another one
 
 console.log(phoneNumber); */
+
+
+
+// SLICE METHOD ( String Slicing)
+
+/* slice() extracts a section of a String
+and returns it as a new String,
+without modifying the original String.. */
+
+
+let fullName = "Snoop Dog";
+
+let firstName;
+let lastName;
+
+/* firstName = fullName.slice(0,6)
+lastName = fullName.slice(7); */
+
+firstName = fullName.slice(0, fullName.indexOf(" "));
+lastName = fullName.slice(fullName.indexOf(" ") +1);
+
+console.log(firstName);
+console.log(lastName);
