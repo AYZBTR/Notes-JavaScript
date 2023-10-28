f8ec6f3d6266818e661a1f267b9fa90d1af1a085
Aayush Bhattarai <bhattaraiaayush.78@gmail.com>
2023-10-29 00:09:52 +0300

    Checkbox property  simple exercise

--- index.js
+++ index.js
@@ -293,5 +293,18 @@ else{
 } */
 
 
-//
+//CHECKED PROPERTY exercise.
+document.getElementById("myButton").onclick = function(){
+
+    const myCheckBox = document.getElementById("myCheckBox");
+
+    if(myCheckBox.checked == true){
+        console.log("You are subscribed");
+
+    }
+    else{
+        console.log("You are not subscribed!!");
+    }
+
+}
 
