6431063d0e8123af6d035277a801a07be2a82f77
Aayush Bhattarai <bhattaraiaayush.78@gmail.com>
2023-11-12 02:21:46 +0200

    !

--- index.js
+++ index.js
@@ -723,3 +723,11 @@ document.getElementById("myLabel").innerHTML = text;
 
 
 //Very basic number guessing game
+
+const answer = Math.floor(Math.random() * 10 +1)
+let guesses = 0;
+
+document.getElementById("submitButton").onclick = function (){
+
+    let guess = document.getElementById("guessField").value
+}
